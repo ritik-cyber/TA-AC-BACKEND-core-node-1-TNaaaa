4cc2ddf775f1cf903e754f5e4966d6061de83280
ritik-cyber <ritikdhingra03@gmail.com>
2021-12-11 12:54:59 +0530

    done

--- block-BNaaag/inde.js
+++ block-BNaaag/inde.js
@@ -1,2 +1,11 @@
-var fs = require("fs");
-fs.readFile("./content.md");
+var { readFile, readFileSync } = require("fs");
+
+readFile("./content.md", "utf8", (err, content) => {
+  console.log(content);
+});
+var result = readFileSync("./content.md", "utf8");
+console.log(result);
+
+var buff1 = Buffer.alloc(10);
+buff1.write("welocme to nodejs");
+console.log(buff1);
