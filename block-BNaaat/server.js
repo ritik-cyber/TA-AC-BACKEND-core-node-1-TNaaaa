ec51706b636c579318c274083c795325073c4b78
ritik-cyber <ritikdhingra03@gmail.com>
2021-12-13 12:15:33 +0530

    donr

---
+++ block-BNaaat/server.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer(handlerServer);
+
+function handlerServer(req, res) {
+  if (req.method === "GET" && req.url === "/file") {
+    fs.readFile("./node.html", (err, content) => {
+      if (err) console.log(err);
+      res.end(content);
+    });
+  } else if (req.method === "GET" && req.url === "/stream") {
+    fs.createReadStream("/node.html").pipe(res);
+  }
+}
+
+server.listen(5555, () => {
+  console.log("ready to port onoo 5555");
+});
